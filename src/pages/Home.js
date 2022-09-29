e036942193792a1a9d4feea2d5c11e2685368466
Trung Lam <trunglam107@gmail.com>
2022-09-28 23:06:57 -0700

    updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,9 +11,9 @@ const Home = () => {
       <div className='home-text'>
         <p>Hi!</p>
         <p>My name is <span>Trung Lam</span></p>
-        <p>I am a <span>Generalist Programmer</span></p>
-        <p><span>HTML, CSS, JavaScript, Python, C++</span></p>
-        <p><span>Unity, Godot</span></p>
+        <p>I am a <span>Front-End Web Developer</span></p>
+        <p>Languages I know: <span>HTML, CSS, JavaScript, Python, C++</span></p>
+        <p>Tools I know: <span>Unity, Godot, React.js</span></p>
       </div>
     </div>
   )
